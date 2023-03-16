01dd9226d092fa6bf8d03f8eb317047310dfd872
PonlawatP <ict45150@gmail.com>
2023-03-17 06:22:53 +0700

    foods menu implement

--- src/app/components/food-card/food-card.component.ts
+++ src/app/components/food-card/food-card.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-food-card',
   templateUrl: './food-card.component.html',
-  styleUrls: ['./food-card.component.scss']
+  styleUrls: ['./food-card.component.scss'],
 })
 export class FoodCardComponent implements OnInit {
+  @Input() data = { id: 0, name: '', type: '', price: 0, img: '' };
+  temp_status = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -8,14 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
   menuData: any;
+  menuContent: any;
+  faArrowUp = faArrowUp;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private route: ActivatedRoute) {
     let url = 'http://localhost/plutosteak-api/menu';
     this.http.get(url).subscribe((data: any) => {
-      console.log(data);
       this.menuData = Object.keys(data);
+      this.menuContent = data;
     });
   }
 
   ngOnInit(): void {}
+  upTop() {
+    window.document.body.scrollTop = 0;
+    window.document.documentElement.scrollTop = 0;
+  }
 }
